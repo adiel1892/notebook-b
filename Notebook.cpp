f3fffacd08c27b8b4ea44cea176b422f2e511e63
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-21 19:48:10 +0200

    works

--- Notebook.cpp
+++ Notebook.cpp
@@ -3,7 +3,7 @@
 using namespace ariel;
 
 
-void Notebook::write(unsigned int page, unsigned int row, unsigned int column, Direction d ,string const str){
+void Notebook::write(unsigned int page, unsigned int row, unsigned int column, Direction d ,string const& str){
     if(column > MAX_COLUMN || str.length() > MAX_COLUMN || (column + str.length()) > MAX_COLUMN || row < 0 || column < 0 || page < 0){
         __throw_invalid_argument("page , row , column str.length must be >= 0. \n Make sure the column and the string less than 100.");
     }
