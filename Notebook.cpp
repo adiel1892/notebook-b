7646cfcd3444ba539ee987fa3b03397b445800e8
Adiel Ben Meir <adiel1892@gmail.com>
2022-03-22 16:43:10 +0200

    change

--- Notebook.cpp
+++ Notebook.cpp
@@ -2,21 +2,37 @@
 #include "Notebook.hpp"
 using namespace ariel;
 
+Notebook::Notebook(){
+    
+}
+
 
 void Notebook::write(unsigned int page, unsigned int row, unsigned int column, Direction d ,string const& str){
-    if(column > MAX_COLUMN || str.length() > MAX_COLUMN || (column + str.length()) > MAX_COLUMN || row < 0 || column < 0 || page < 0){
+    if(column > MAX_COLUMN || str.length() > MAX_COLUMN || row < 0 || column < 0 || page < 0){
         __throw_invalid_argument("page , row , column str.length must be >= 0. \n Make sure the column and the string less than 100.");
+    }
+    if(d == Direction::Horizontal){
+        if(column + str.length() > 100){
+            __throw_invalid_argument("Make sure the column and the string less than 100.");
+        }
+        
+        // for(int i = 0; i < str.length(); i++){
+        //     if()
+        // }
+
     }
 
 }
 string Notebook::read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length){
-    if(column > MAX_COLUMN || length > MAX_COLUMN || (column + length) > MAX_COLUMN || row < 0 || column < 0  || page < 0){
+    if(column > MAX_COLUMN || length > MAX_COLUMN || row < 0 || column < 0  || page < 0){
         __throw_invalid_argument("page , row , column str.length must be >= 0. \n Make sure the column and the string less than 100.");
     }
+    char txt[100];
+    this->book[page][row] = txt;
     return "notebook-a";
 }
 void Notebook::erase(unsigned int page, unsigned int row, unsigned int column,Direction d ,unsigned int length){
-    if(column > MAX_COLUMN || length > MAX_COLUMN || (column - length) > MAX_COLUMN || row < 0 || column < 0  || page < 0){
+    if(column > MAX_COLUMN || length > MAX_COLUMN || row < 0 || column < 0  || page < 0 || (row - length) < 0){
         __throw_invalid_argument("page , row , column str.length must be >= 0. \n Make sure the column and the string less than 100.");
     }
 }
--- Notebook.hpp
+++ Notebook.hpp
@@ -3,18 +3,17 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
-#include <map>
+#include <unordered_map>
 using namespace std;
 #define MAX_COLUMN 100
 namespace ariel{
     class Notebook{
-        private:
-        map<unsigned int, map<unsigned int, char[100]>> book;
-        
         public:
-        static void write(unsigned int page, unsigned int row, unsigned int column, Direction d , string const& str);
-        static string read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length);
-        static void erase(unsigned int page, unsigned int row, unsigned int column,Direction d ,unsigned int length);
+        void write(unsigned int page, unsigned int row, unsigned int column, Direction d , string const& str);
+        string read(unsigned int page, unsigned int row, unsigned int column, Direction d ,unsigned int length);
+        void erase(unsigned int page, unsigned int row, unsigned int column,Direction d ,unsigned int length);
         void show(unsigned int page);
+        Notebook();
+        unordered_map<unsigned int, unordered_map<unsigned int, char[100]>> book;
     };
 }
